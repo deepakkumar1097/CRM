31836dfbf654f68c83dd175ad0fcc9b6944edf6d
Deepak <itisdeepakkumar@outlook.com>
2023-08-01 23:22:46 +0530

    updated using findOne and update method

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -23,24 +23,53 @@ exports.findAll = async (req, res) => {
 
 exports.update = async (req, res) => {
   // try using findOneandUpdate
+
   try {
-    const user = await User.findOne({ userId: req.params.id });
+    // Check if the required fields are provided in the request body
+    // if (!req.body.name || !req.body.userType || !req.body.userStatus) {
+    //   return res.status(400).send({
+    //     message: "Name, userType, and userStatus are required fields",
+    //   });
+    // }
+
+    const updatedUser = await User.findOneAndUpdate(
+      { userId: req.params.id }, // Filter criteria to find the user to update
+      {
+        $set: {
+          name: req.body.name,
+          userType: req.body.userType,
+          userStatus: req.body.userStatus,
+          // Add other fields to update if needed
+        },
+      },
+      { new: true } // Return the updated user instead of the old one
+    );
+
+    //const user = await User.findOne({ userId: req.params.id });
     /**
      * Find user which needs updation
      */
-    if (!user) {
+    if (!updatedUser) {
       return res.status(404).send({
         message: `User with the given ID ${req.params.id} is not found`,
       });
     }
 
-    user.name = req.body.name != undefined ? req.body.name : user.name;
-    user.userType =
-      req.body.userType != undefined ? req.body.userType : user.userType;
-    user.userStatus =
-      req.body.userStatus != undefined ? req.body.userStatus : user.userStatus;
+    // user.name = req.body.name != undefined ? req.body.name : user.name;
+    // user.userType =
+    //   req.body.userType != undefined ? req.body.userType : user.userType;
+    // user.userStatus =
+    //   req.body.userStatus != undefined ? req.body.userStatus : user.userStatus;
 
-    const updatedUser = await user.save();
+    updatedUser.name =
+      req.body.name != undefined ? req.body.name : updatedUser.name;
+    updatedUser.userType =
+      req.body.userType != undefined ? req.body.userType : updatedUser.userType;
+    updatedUser.userStatus =
+      req.body.userStatus != undefined
+        ? req.body.userStatus
+        : updatedUser.userStatus;
+    //const updatedUser = await user.save();
 
     res.status(200).send({
       message: "User Updated SuccessFully",
